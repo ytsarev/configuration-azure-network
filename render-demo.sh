e0c5f8ea7308ece3b43dd5e87c005b3ccb48c5d9
Yury Tsarev <yury@upbound.io>
2025-07-31 14:06:11 +0200

    Embed render and validation demos
    Signed-off-by: Yury Tsarev <yury@upbound.io>

---
+++ render-demo.sh
@@ -0,0 +1,1 @@
+crossplane render examples/network-xr.yaml apis/pat/composition.yaml examples/functions.yaml
---
+++ validate-demo.sh
@@ -0,0 +1,1 @@
+crossplane render examples/network-xr.yaml apis/pat/composition.yaml examples/functions.yaml --include-full-xr | crossplane beta validate validate -
